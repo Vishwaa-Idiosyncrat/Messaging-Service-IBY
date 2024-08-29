9dde2869881b2054f9f24719970cc49887d1eecb
Vishwaa <vishwaaps3@gmail.com>
2024-08-29 10:47:22 +0530

    Updates

--- messenger/app/layout.tsx
+++ messenger/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import ToasterContext from "./context/ToasterContext"
+import AuthContext from "./context/AuthContext"
 import { Inter } from "next/font/google";
 import "./globals.css";
 
@@ -18,8 +19,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <ToasterContext />
-        {children}
+        <AuthContext>
+          <ToasterContext />
+          {children}
+        </AuthContext>
         </body>
     </html>
   );
